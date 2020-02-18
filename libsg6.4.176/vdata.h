48cc7b99f9f5d094af13a44d257a087ae0471a8a
ylcangel <ylcangel@gmail.com>
2020-02-18 13:35:51 +0800

    modify vdata.h

--- libsg6.4.176/vdata.h
+++ libsg6.4.176/vdata.h
@@ -28,3 +28,46 @@ struct dcryptdata {
 	struct vdata* key;
 	struct vdata* cryptedata;
 };
+
+struct tmp1_nest_struct {
+	void* tmp1_nf1;
+	void* tmp1_nf2;
+	int len;
+};
+
+struct tmp1_vdata {
+	struct data* datalist; // 指向16bitstruct的數組指針
+	int chunk_count;
+	int chunk_size;
+	void* tmp1_f1;
+	void* tmp1_f2;
+	void* tmp1_f3;
+	struct tmp1_nest_struct tmp1_nest;
+};
+
+struct tmp_vdata { // 創建tmp1_vdata時用到
+	char* wh;
+	void* f1;
+	void* f2;
+};
+
+struct $8bitstruct {
+	int count;
+	struct tmp1_vdata* vdata;
+};
+
+struct $24bitstruct {
+	int a; // 1
+	int b; // 9
+	long time;
+	int c;
+	struct tmp1_vdata* vdata; // tmp1_vdata4
+	int d;
+};
+
+struct $16bitstruct {
+	int t;
+	int lrand;
+	int random;
+	int sec;
+};
